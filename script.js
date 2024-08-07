f839fdf3ba4325904f5b3108899012ace7127f58
Anano Makharashvili <ananomakharashvili@gmail.com>
2024-08-07 19:10:17 +0300

    corrected

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const getTogglerCircle = document.getElementById("toggler-circle");
-const getButton = document.getElementById("toggler-button");
+const getToggleCircle = document.getElementById("toggle-circle");
+const getButton = document.getElementById("toggle-button");
 console.log(getButton);
 
-getTogglerCircle.addEventListener("click", () => {
-  getTogglerCircle.style.marginLeft = "18";
+getToggleCircle.addEventListener("click", () => {
+  getToggleCircle.style.marginLeft = "18";
   getButton.style.backgroundColor = "#7AEADF";
 });
