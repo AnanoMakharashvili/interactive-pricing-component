e736a75e7ed7bf6e10f3d313607c656588a28d8f
Anano Makharashvili <ananomakharashvili@gmail.com>
2024-08-10 19:53:01 +0300

    corrected

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 const getToggleCircle = document.getElementById("toggle-circle");
 const getButton = document.getElementById("toggle-button");
-console.log(getButton);
+const getInputRange = document.getElementById("range");
+const pageviewCost = document.getElementById("pageview");
+const getPrice = document.getElementById("price");
 
-getToggleCircle.addEventListener("click", () => {
+function calculateCost(value) {
+  let cost = 8;
+
+  if ((value >= 0) & (value <= 20)) {
+    pageviewCost.textContent = "10K Pageviews";
+    cost = 8;
+  } else if (value > 20 && value <= 40) {
+    pageviewCost.textContent = "50K Pageviews";
+    cost = 12;
+  } else if (value > 40 && value <= 60) {
+    pageviewCost.textContent = "100K Pageviews";
+    cost = 16;
+  } else if (value > 60 && value <= 80) {
+    pageviewCost.textContent = "500K Pageviews";
+    cost = 24;
+  } else if (value > 80 && value <= 100) {
+    pageviewCost.textContent = "1M Pageviews";
+    cost = 36;
+  }
+}
+
+getInputRange.addEventListener("input", (event) => {
+  event.target.value;
+  const value = range.value;
+  calculateCost(value);
+});
+
+getToggleCircle.addEventListener("input", (event) => {
+  event.target.value;
+  const value = range.value;
   getToggleCircle.style.marginLeft = "18";
   getButton.style.backgroundColor = "#7AEADF";
+  calculateCost(value);
 });
